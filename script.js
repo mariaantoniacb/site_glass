d372d3862baca4be51589c829acc968deac66bf9
Maria Antônia <mariaantoniabiacb@gmail.com>
2026-03-13 16:03:48 -0300

    pequenas atualizações mo site
    arrumei a barra de rolagem da seção de "designers para ficar de olho"

--- script.js
+++ script.js
@@ -1,25 +1,64 @@
+// Menu hamburguer
+
+// function clickMenu(){
+//     var menu = document.getElementById('menu')
+//     menu.style.display = 'block'
+//     if(menu.style.display == 'block' ){
+//         menu.style.display = 'none'
+//     }else {
+//         menu.style.display = 'block'
+//     }
+// }
+
+
+
+
+
+
+
+//  pag inicial - seção "Designers para ficar de olho"
+
 const container = document.getElementById("designersCards");
 const btnLeft = document.querySelector(".button-scroll.left");
 const btnRight = document.querySelector(".button-scroll.right");
-
-
-
-btnRight.addEventListener("click", () => {
-    container.scrollLeft += 300;
-});
-
-btnLeft.addEventListener("click", () => {
-    container.scrollLeft -= 300;
-});
 
 container.innerHTML += container.innerHTML;
 
 let scrollSpeed = 0.5;
 let position = 0;
+let autoscroll = true;
+
+btnRight.addEventListener("click", () => {
+    autoscroll = false;
+    container.scrollLeft += 300;
+    position = container.scrollLeft;
+
+    setTimeout(() => {
+        autoscroll = true;
+    }, 2000);
+});
+
+btnLeft.addEventListener("click", () => {
+    autoscroll = false;
+    container.scrollLeft -= 300;
+    position = container.scrollLeft;
+
+    setTimeout(() => {
+        autoscroll = true;
+    }, 2000);
+});
+
+
+
+
 
 function infiniteScroll() {
+   
+    if (autoscroll) {
     position += scrollSpeed;
     container.scrollLeft = position;
+    }
+   
 
 
     if (position >= container.scrollWidth / 2) {
